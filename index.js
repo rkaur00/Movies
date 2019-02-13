f8f7e9d1fdf561905e8991f07bcbe04090aaa08b
user <user@users-MBP.hitronhub.home>
2019-02-13 12:07:10 -0800

    movielist assignment

---
+++ index.js
@@ -0,0 +1,25 @@
+
+const Koa = require('koa')
+const bodyParser = require('koa-bodyparser')
+const pool = require('./db/db')
+
+const app = new Koa()
+app.use(bodyParser())
+
+
+app.use(async ctx => {
+  const dbtitle = await ctx.request.body.title
+  const item = await show(dbtitle)
+  ctx.body = item
+})
+
+async function show(title) {
+  try {
+    const itemData = await pool.query(`SELECT movieID, movieTitle, IF(movieStatus,'true','false')movieStatus FROM movieList WHERE movieTitle  LIKE '%${title}%'`)
+    return itemData[0]
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+module.exports = app.callback()
